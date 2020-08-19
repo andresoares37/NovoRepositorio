0f8b44cbf22f1b6decb71c4b0cf47aa2619bfb1f
andresoares37 <30959916+andresoares37@users.noreply.github.com>
2020-08-18 21:59:16 -0300

    subindo arquivos API

---
+++ Controllers/ControleAula1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API_Aula1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Aula1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ControleAula1 : ControllerBase
+    {
+        [HttpGet]
+        public Pessoa MetodoGet ()
+        {
+            
+            Pessoa pessoa = new Pessoa();
+            pessoa.ID = 1;
+            pessoa.Nome = "Bruno";
+            pessoa.Idade = 20;
+            return pessoa;
+
+        }
+        [HttpPost]
+        public string MetodoPost([FromBody] Pessoa pessoa)
+        {
+            pessoa.ID = 1;
+            return $"{pessoa.ID} ? MetodoPost - OK";
+        }
+
+        [HttpPut]
+        public Pessoa MetodoPut(int id)
+        {
+            Pessoa pessoa = new Pessoa();
+            pessoa.ID = 2;
+            pessoa.Nome = "Carol";
+            pessoa.Idade = 25;
+
+            return pessoa;
+
+        }
+        [HttpDelete]
+        public string MetodoDelete(int id)
+        {
+            return "Metodo Delete - OK";
+        }
+
+    }
+}
---
+++ Models/Pessoa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Aula1.Models
+{
+    public class Pessoa
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+
+        public int Idade { get; set; }
+    }
+}
